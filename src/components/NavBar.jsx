5fbfe556f8085a40b1177e06d1d2fba85fd91a83
zubairkhawar <zubairkhawer@gmail.com>
2024-04-09 06:51:22 +0500

    update navbar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,36 +1,75 @@
-import React, { useState, useEffect }from 'react'
+import React, { useState } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import CloseIcon from '@mui/icons-material/Close';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 const NavBar = () => {
-  return (
-    <div>
-      <MenuIcon />
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
 
-      <ul>
+  const toggleMobileNav = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+  };
+
+  return (
+    <div className='w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-gray-600'>
+      <div className='flex ml-4'>
+        <h1 className='text-bold text-xl'>Haramain Abrasives</h1>
+      </div>
+      <ul className='hidden lg:flex px-4 space-x-6'>
         <li>
-            <a href='#'>Home</a>
+          <a href='/'>Home</a>
         </li>
         <li>
-            <a href='#'>About Us</a>
+          <a href='/aboutus'>About Us</a>
         </li>
         <li>
-            <a href='#'>Products</a>
+          <a href='/products'>Products</a>
         </li>
         <li>
-            <a href='#'>Blogs</a>
+          <a href='/blogs'>Blogs</a>
         </li>
         <li>
-            <a href='#'>Contact Us</a>
-        </li>
-        <li>
-        <SearchIcon />
-        <CloseIcon />
+          <a href='/contactus'>Contact Us</a>
         </li>
       </ul>
-    </div>
-  )
-}
+      <div className='lg:hidden'>
+        <MenuIcon fontSize='large' className='mr-4 cursor-pointer' onClick={toggleMobileNav} />
+      </div>
 
-export default NavBar
+      {/* Mobile Nav*/}
+      {isMobileNavOpen && (
+        <div className={`w-[90%] max-w-[400px] overflow-hidden lg:hidden ease-in duration-500 absolute text-black right-0 top-0 h-screen bg-white transform flex flex-col ${
+            isMobileNavOpen ? 'translate-x-0 ' : 'translate-x-full'
+          }`}>
+          <div>
+            <CloseIcon fontSize='large' className='absolute right-5 top-5 cursor-pointer' onClick={toggleMobileNav} />
+          </div>
+
+          <ul className='h-full w-full pt-12 pl-6'>
+            <li className='text-3xl py-3'>
+              <a href='/'>Home</a>
+            </li>
+            <li className='text-3xl py-3'>
+              <a href='/aboutus'>About Us</a>
+            </li>
+            <li className='text-3xl py-3 flex justify-between'>
+              <a href='/products'>Products</a>
+              <div>
+                <KeyboardArrowDownIcon fontSize='large' className='pb-1 mr-5' />
+              </div>
+            </li>
+            <li className='text-3xl py-3'>
+              <a href='/blogs'>Blogs</a>
+            </li>
+            <li className='text-3xl py-3'>
+              <a href='/contactus'>Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
