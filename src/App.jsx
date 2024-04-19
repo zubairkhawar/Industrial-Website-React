a290a33eae04b29ab7351b051741feca8367f1e3
zubairkhawar <zubairkhawer@gmail.com>
2024-04-19 07:49:37 +0500

    custom 404 error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router,Routes,Route,} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TopBar from './components/TopBar';
 import Contact from './pages/Contact';
 import Aboutus from './pages/Aboutus';
 import Home from './pages/Home';
-import Products from './pages/Products'
+import Products from './pages/Products';
 import CoatedAbrasives from './pages/CoatedAbrasives';
 import DiamondBlades from './pages/DiamondBlades';
 import SteelGrindingDisc from './pages/SteelGrindingDisc';
 import FlapDisc from './pages/FlapDisc';
 import Faqs from './pages/Faqs';
 import Blog from './pages/Blog';
+import NotFound from './pages/NotFound';
 
 function App() {
-  
   return (
     <Router>
-      <TopBar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/aboutus" element={<Aboutus />} />
@@ -28,6 +27,7 @@ function App() {
         <Route path="/steelgrindingdisc" element={<SteelGrindingDisc />} />
         <Route path="/flapdisc" element={<FlapDisc />} />
         <Route path="/faqs" element={<Faqs />} />
+        <Route path="*" element={<NotFound />} /> {/* This will match any other route */}
       </Routes>
     </Router>
   );
